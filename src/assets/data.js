3257b627dea946b06886c38a4a279ac6378c7a21
Oduri-Naresh <narasimhavodhuri@gmail.com>
2025-06-30 16:48:59 +0530

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,45 @@
+// quizData.js
+ export const quizData = [
+  {
+    question: "What is the purpose of React's useState hook?",
+      option1: "To directly modify the DOM",
+      option2: "To store and update state in a functional component",
+      option3: "To fetch data from an API",
+      option4: "To create global variables",
+      ans: 2,
+  },
+  {
+    question: "Which method is used to render React elements to the DOM?",
+    
+      option1: "React.mount()",
+      option2: "ReactDOM.render()",
+      option3: "React.render()",
+      option4: "ReactDOM.mount()",
+      ans: 3,
+  },
+  {
+    question: "What is JSX?",
+      option1: "A templating engine",
+      option2: "A file format",
+      option3: "A syntax extension for JavaScript",
+      option4: "A backend framework",
+    ans: 3,
+  },
+  {
+    question: "Which hook is used for side effects in React?",
+      option1:"useState",
+      option2:"useEffect",
+      option3:"useContext",
+      option4:"useReducer",
+    ans:2,
+  },
+  {
+    question: "What does a key prop help React identify?",
+      option1:"Which items are changed, added, or removed",
+      option2: "Which CSS file to apply",
+      option3:"Which component is global",
+      option4:"The state value",
+    ans: 1,
+  }
+];
+
